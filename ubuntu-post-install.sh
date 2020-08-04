91076804064c05758acd6b892781614c3f71a8f5
thlin <brad19970819@gmail.com>
2020-08-04 20:00:31 +0800

    Add apt packages and post-installation script

---
+++ ubuntu-post-install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt update
+sudo install -y `cat packages/apt` 
+sudo ufw allow ssh
+
+# python related
+# upgrade python3 to python3.8
+sudo apt install -y python3.8
+sudo update-alternatives --install $(which python3) python3 $(which python3.8) 1
+
+# python3 pip
+sudo apt install -y python3-pip
+
+# python3 pipenv
+pip3 install pipenv
+
+# rc file settings
+git clone https://github.com/BradLin0819/dotfiles.git && \
+cp .bashrc .bashrc.bak && cp dotfiles/.* 
+source ~/.bashrc
